bed4b80a682cedfc16424c0912f63118b729f08d
Waggerok <115732069+Waggerok@users.noreply.github.com>
2025-02-18 16:07:36 +0300

    connection to database

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
+require('dotenv').config();
 const express = require('express');
+const sequelize = require('./data/database');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-// const start = () => {
-//     try {
-        
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
+const start = async() => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, () => console.log(`Server is running on port${PORT}`));
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-app.listen(PORT, () => console.log(`Server is running on port${PORT}`));
-
+start();
