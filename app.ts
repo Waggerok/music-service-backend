4dae2304ca74025d31388399b63fdbd61ed78379
Waggerok <115732069+Waggerok@users.noreply.github.com>
2025-02-19 11:57:21 +0300

    added cors

--- app.ts
+++ app.ts
@@ -2,12 +2,15 @@ import dotenv from 'dotenv';
 import express from 'express';
 const models = require('./models/models');
 import {sequelize} from './data/database';
-import userRouter from './routes/userRouter'
+import userRouter from './routes/userRouter';
+import cors from 'cors'
 
 dotenv.config();
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 5000
 const app = express();
+
+app.use(cors())
 
 app.use(express.json())
 app.use('/api/user', userRouter)
